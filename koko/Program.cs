77cfdec0e79633abe83da48ab89782ab5770f008
Folk2299 <jakraphat.j@kkumail.com>
2024-11-25 20:46:18 -0800

    ddd

---
+++ koko/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Jakraphat Juntabool";
+string student_id = "673450186-5";
+// string is Datayype -> ชนิดของข้อมูล
+// name is variable
+// "..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("My student id: 673450186-5");
